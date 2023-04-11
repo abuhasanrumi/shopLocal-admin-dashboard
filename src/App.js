0434acdb8659e4536936a8cbbec7ddffae9237c6
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-12 01:12:26 +0600

    working on making basic dashboard list structures

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Login from './pages/Login';
 import ResetPassword from './pages/ResetPassword';
 import ForgotPassword from './pages/ForgotPassword';
 import MainLayout from './components/MainLayout';
+import Enquiries from './pages/Enquiries';
+import Bloglist from './pages/Bloglist';
+import BlogCatList from './pages/BlogCatList';
+import Orders from './pages/Orders';
+import Customers from './pages/Customers';
+import ColorList from './pages/ColorList';
+import ProdCatList from './pages/ProdCatList';
+import BrandList from './pages/BrandList';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
@@ -15,6 +24,15 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blogList" element={<Bloglist />} />
+          <Route path="blogCategoryList" element={<BlogCatList />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="colorList" element={<ColorList />} />
+          <Route path="categoryList" element={<ProdCatList />} />
+          <Route path="brandList" element={<BrandList />} />
+          <Route path="productList" element={<ProductList />} />
         </Route>
       </Routes>
     </Router>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,179 @@
 import React from 'react'
+import { BsArrowDownRight, BsArrowUpRight } from "react-icons/bs";
+import { Column } from "@ant-design/plots";
+import { Table } from "antd";
+
+const columns = [
+    {
+        title: "SNo",
+        dataIndex: "key",
+    },
+    {
+        title: "Name",
+        dataIndex: "name",
+    },
+    {
+        title: "Product",
+        dataIndex: "product",
+    },
+    {
+        title: "Status",
+        dataIndex: "staus",
+    },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+    data1.push({
+        key: i,
+        name: `Edward King ${i}`,
+        product: 32,
+        staus: `London, Park Lane no. ${i}`,
+    });
+}
 
 const Dashboard = () => {
+    const data = [
+        {
+            type: "Jan",
+            sales: 38,
+        },
+        {
+            type: "Feb",
+            sales: 52,
+        },
+        {
+            type: "Mar",
+            sales: 61,
+        },
+        {
+            type: "Apr",
+            sales: 145,
+        },
+        {
+            type: "May",
+            sales: 48,
+        },
+        {
+            type: "Jun",
+            sales: 38,
+        },
+        {
+            type: "July",
+            sales: 38,
+        },
+        {
+            type: "Aug",
+            sales: 38,
+        },
+        {
+            type: "Sept",
+            sales: 38,
+        },
+        {
+            type: "Oct",
+            sales: 38,
+        },
+        {
+            type: "Nov",
+            sales: 38,
+        },
+        {
+            type: "Dec",
+            sales: 38,
+        },
+    ];
+    const config = {
+        data,
+        xField: "type",
+        yField: "sales",
+        color: ({ type }) => {
+            return "#ffd333";
+        },
+        label: {
+            position: "middle",
+            style: {
+                fill: "#FFFFFF",
+                opacity: 1,
+            },
+        },
+        xAxis: {
+            label: {
+                autoHide: true,
+                autoRotate: false,
+            },
+        },
+        meta: {
+            type: {
+                alias: "Month",
+            },
+            sales: {
+                alias: "Income",
+            },
+        },
+    };
     return (
-        <div>Dashboard</div>
+        <div>
+            <h3 className="mb-4 title">Dashboard</h3>
+            <div className="">
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-4 mb-3">
+                        <div className="bg-white p-4 d-flex justify-content-between">
+                            <div>
+                                <p className="desc text-muted">Total sells</p>
+                                <h2 className="mb-0 mt-3 sub-title">$1100.00</h2>
+                            </div>
+                            <div className="d-flex flex-column align-items-end justify-content-end">
+                                <h6 className="red">
+                                    <BsArrowDownRight /> 32%
+                                </h6>
+                                <p className="mb-0 desc text-muted d-none d-lg-block">Compared To April 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4 mb-3">
+                        <div className="bg-white p-4 d-flex justify-content-between">
+                            <div>
+                                <p className="desc text-muted">Total sells</p>
+                                <h2 className="mb-0 mt-3 sub-title">$1100.00</h2>
+                            </div>
+                            <div className="d-flex flex-column align-items-end justify-content-end">
+                                <h6 className="red">
+                                    <BsArrowDownRight /> 32%
+                                </h6>
+                                <p className="mb-0 desc text-muted d-none d-lg-block">Compared To April 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4 mb-3">
+                        <div className="bg-white p-4 d-flex justify-content-between">
+                            <div>
+                                <p className="desc text-muted">Total sells</p>
+                                <h2 className="mb-0 mt-3 sub-title">$1100.00</h2>
+                            </div>
+                            <div className="d-flex flex-column align-items-end justify-content-end">
+                                <h6 className="red">
+                                    <BsArrowDownRight /> 32%
+                                </h6>
+                                <p className="mb-0 desc text-muted d-none d-lg-block">Compared To April 2022</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-4">
+                <h3 className="mb-5 title">Income Statics</h3>
+                <div>
+                    <Column {...config} />
+                </div>
+            </div>
+            <div className="mt-4">
+                <h3 className="mb-5 title">Recent Orders</h3>
+                <div>
+                    <Table columns={columns} dataSource={data1} />
+                </div>
+            </div>
+        </div>
     )
 }
 
